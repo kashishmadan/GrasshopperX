992faf3693ae80c48d1c72ad9fcfb830ef09ea2c
kashishmadan <40457149+kashishmadan@users.noreply.github.com>
2019-08-07 14:00:08 +0530

    initial c

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import "package:flutter/material.dart";
-import 'loader.dart';
 import 'dart:async';
 import 'homescreen.dart';
 void main() {
@@ -35,59 +34,19 @@ class _SplashScreenState extends State<SplashScreen>{
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Scaffold(
-    body:
-      Stack(
-      fit: StackFit.expand,
-      children: <Widget>[
-        Container(
-          decoration: BoxDecoration(
-              gradient: LinearGradient(
-                begin: Alignment.topCenter,
-                end: Alignment.bottomCenter,
-                colors: [Colors.lightBlue[900],Colors.lightBlue[800],Colors.lightBlue[700],Colors.lightBlue[600],Colors.lightBlue[400],
-                  Colors.lightBlue[300],Colors.lightBlue[200],Colors.lightBlue[100],]
-              ),
-          ),
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+            image: AssetImage('assets/home_image_grasshopperx.jpeg'),
+            fit: BoxFit.cover
+        ) ,
+      ),
+      child: Center(
+        child: CircularProgressIndicator(
+          valueColor: AlwaysStoppedAnimation<Color>(Colors.redAccent),
         ),
-        Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            Expanded(
-              flex: 2,
-              child: Container(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    CircleAvatar(
-                      backgroundColor: Colors.white,
-                      radius: 50.0,
-                      child: Icon(Icons.computer,
-                      color: Colors.deepPurple,
-                      size: 50.0,),
-                    ),
-                    Text("Xpense",style: TextStyle(color: Colors.white,fontSize: 50, fontWeight:FontWeight.bold ),),
-                    Padding(padding: EdgeInsets.only(top: 1.0))
-                  ],
-                ),
-              ),
-            ),
-            Text("Your personal expense manager",style: TextStyle(color: Colors.white,fontSize: 18.0),),
-            Expanded(
-              flex: 1,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Loader(),
-                  Padding(padding: EdgeInsets.only(top: 20.0))
-
-                ],
-              ),
-            ),
-          ],
-        )
-      ],
-    )
+      ),
     );
-  }
+
+
 }

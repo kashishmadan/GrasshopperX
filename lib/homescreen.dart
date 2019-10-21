8657d474b6b998db245104371842b4fe4f0d2b9d
kashishmadan <40457149+kashishmadan@users.noreply.github.com>
2019-10-22 03:51:15 +0530

    Added Items

--- lib/homescreen.dart
+++ lib/homescreen.dart
@@ -13,6 +13,14 @@ class _HomeScreenState extends State<HomeScreen> {
         title: Text('Welcome, Kashish!'),
         backgroundColorStart: Colors.lightBlue[900],
         backgroundColorEnd: Colors.blue[900],
+        actions: <Widget>[
+          FlatButton.icon(
+            textColor: Colors.white,
+            onPressed: (){},
+            icon: Icon(Icons.star,size: 40.0,color: Colors.yellow,),
+            label: Text('9',style: TextStyle(fontSize: 30)),
+          )
+        ],
       ),
       body: Container(
         color: Colors.indigo[900],
@@ -20,60 +28,104 @@ class _HomeScreenState extends State<HomeScreen> {
 
           children: <Widget>[
             Padding(
-              padding: const EdgeInsets.only(left:50.0,top:10.0,right:15.0,bottom:20.0 ),
+              padding: const EdgeInsets.only(left:200.0,top:10.0,right:20.0,bottom:20.0 ),
               child: FlatButton.icon(
                   color: Colors.white70,
-                  highlightColor: Colors.blue,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: new BorderRadius.circular(18.0),
+                  ),
                   onPressed:(){},
-                  icon: Icon(Icons.child_friendly), label: Text('Beginner Code'))
+                  icon: Icon(Icons.flag,color: Colors.orange,), label: Text('French Flag',style: TextStyle(fontSize: 20),),)
             ),
             Padding(
-                padding: const EdgeInsets.only(left:15.0,top:10.0,right:50.0,bottom:20.0 ),
+                padding: const EdgeInsets.only(left:100.0,top:10.0,right:100.0,bottom:20.0 ),
                 child: FlatButton.icon(
                     color: Colors.white70,
-                    highlightColor: Colors.blue,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: new BorderRadius.circular(18.0),
+                    ),
                     onPressed:(){},
-                    icon: Icon(Icons.child_friendly), label: Text('Beginner Code'))
+                    icon: Icon(Icons.assignment,color: Colors.green[800],), label: Text('How Many Blue?',style: TextStyle(fontSize: 20),))
             ),
             Padding(
-                padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 0.0),
+                padding: const EdgeInsets.only(left:200.0,top:10.0,right:5.0,bottom:20.0 ),
                 child: FlatButton.icon(
                     color: Colors.white70,
-                    highlightColor: Colors.blue,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: new BorderRadius.circular(18.0),
+                    ),
                     onPressed:(){},
-                    icon: Icon(Icons.child_friendly), label: Text('Beginner Code'))
+                    icon: Icon(Icons.star,color: Colors.yellowAccent,), label: Text('Used a Function',style: TextStyle(fontSize: 20),))
             ),
             Padding(
-                padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 0.0),
+                padding: const EdgeInsets.only(left:20.0,top:10.0,right:100.0,bottom:20.0 ),
                 child: FlatButton.icon(
                     color: Colors.white70,
-                    highlightColor: Colors.blue,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: new BorderRadius.circular(18.0),
+                    ),
                     onPressed:(){},
-                    icon: Icon(Icons.child_friendly), label: Text('Beginner Code'))
+                    icon: Icon(Icons.linear_scale,color: Colors.red,), label: Text('A Dash of Random',style: TextStyle(fontSize: 20),))
             ),
             Padding(
-                padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 0.0),
+                padding: const EdgeInsets.only(left:150.0,top:10.0,right:50.0,bottom:20.0 ),
                 child: FlatButton.icon(
                     color: Colors.white70,
-                    highlightColor: Colors.blue,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: new BorderRadius.circular(18.0),
+                    ),
                     onPressed:(){},
-                    icon: Icon(Icons.child_friendly), label: Text('Beginner Code'))
+                    icon: Icon(Icons.note_add,color: Colors.purpleAccent,), label: Text('Which Drinks?',style: TextStyle(fontSize: 20),))
             ),
             Padding(
-                padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 0.0),
+                padding: const EdgeInsets.only(left:5.0,top:10.0,right:200.0,bottom:20.0 ),
                 child: FlatButton.icon(
                     color: Colors.white70,
-                    highlightColor: Colors.blue,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: new BorderRadius.circular(18.0),
+                    ),
                     onPressed:(){},
-                    icon: Icon(Icons.child_friendly), label: Text('Beginner Code'))
+                    icon: Icon(Icons.cake,color: Colors.pink,), label: Text('Bake a Cake',style: TextStyle(fontSize: 20),))
             ),
             Padding(
-                padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 0.0),
+                padding: const EdgeInsets.only(left:20.0,top:10.0,right:150.0,bottom:20.0 ),
                 child: FlatButton.icon(
                     color: Colors.white70,
-                    highlightColor: Colors.blue,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: new BorderRadius.circular(18.0),
+                    ),
                     onPressed:(){},
-                    icon: Icon(Icons.child_friendly), label: Text('Beginner Code'))
+                    icon: Icon(Icons.check,color: Colors.green[600]), label: Text('Checkerboard',style: TextStyle(fontSize: 20),))
+            ),
+            Padding(
+                padding: const EdgeInsets.only(left:100.0,top:10.0,right:100.0,bottom:20.0 ),
+                child: FlatButton.icon(
+                    color: Colors.white70,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: new BorderRadius.circular(18.0),
+                    ),
+                    onPressed:(){},
+                    icon: Icon(Icons.monetization_on,color: Colors.yellow[900],), label: Text('Flip a Coin',style: TextStyle(fontSize: 20),))
+            ),
+            Padding(
+                padding: const EdgeInsets.only(left:150.0,top:10.0,right:20.0,bottom:0.0 ),
+                child: FlatButton.icon(
+                    color: Colors.white70,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: new BorderRadius.circular(18.0),
+                    ),
+                    onPressed:(){},
+                    icon: Icon(Icons.tag_faces,color: Colors.redAccent,), label: Text('Is It Happy?',style: TextStyle(fontSize: 20),))
+            ),
+            Padding(
+                padding: const EdgeInsets.only(left:15.0,top:10.0,right:100.0,bottom:20.0 ),
+                child: FlatButton.icon(
+                    color: Colors.white70,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: new BorderRadius.circular(18.0),
+                    ),
+                    onPressed:(){},
+                    icon: Icon(Icons.star,color: Colors.deepPurple[800],), label: Text('Used Math Operators',style: TextStyle(fontSize: 20),))
             ),
           ],
         ),
